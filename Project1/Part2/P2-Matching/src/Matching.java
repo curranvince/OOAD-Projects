2308c2339d4d9067985ef71bf9a012b0a50114e4
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2022-01-26 14:50:08 -0700

    added comments

--- Project1/Part2/P2-Matching/src/Matching.java
+++ Project1/Part2/P2-Matching/src/Matching.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Matching {
+    // use same 12 words every time
     static String[] words_ = new String[] {
         "CLASS",
         "FLAGS",
@@ -18,20 +19,29 @@ public class Matching {
     };
 
     static void Run() {
+        // initialize rand and scanner
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
+        // pick the index of the word to be guessed
         int index = rand.nextInt(12);
         boolean cont = true;
         while (cont) {
-            int matches = 0;
+            
+            // ask user for a guess
             System.out.println("Guess a 5-letter word: ");
             String guess = scanner.nextLine();
             if (guess.length() == 0) {
+                // if empty input then exit program
                 System.exit(0);
             } else if (guess.length() != 5) {
+                // if guess length is too big or small report error
                 System.out.println("Please input a 5 letter word next time.");
             } else {
+                int matches = 0;
+                // if we have valid 5 letter word conver to uppercase for comparison
                 guess = guess.toUpperCase();
+                // check each letter of the guess against real word
+                // give information wether each letter is correct, in the wrong position, or not in the word
                 for (int i = 0; i < 5; i++) {
                     if (guess.charAt(i) == words_[index].charAt(i)) {
                         System.out.println(guess.charAt(i) + " is a match in the correct location.");
@@ -42,7 +52,7 @@ public class Matching {
                         System.out.println(guess.charAt(i) + " is not in the word");
                     }
                 }
-
+                // if all 5 letters match then tell user they win and end program
                 if (matches == 5) {
                     System.out.println("You've guessed the correct word! Congratulations!");
                     cont = false;
