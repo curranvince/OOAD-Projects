0adefcb30aa5d4218d6352cf6680328172792ed7
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2022-02-26 13:52:02 -0700

    merged factory files

--- Project4/Part2/FNMSProject/src/main/java/FNMS/ItemFactory.java
+++ Project4/Part2/FNMSProject/src/main/java/FNMS/ItemFactory.java
@@ -8,50 +8,56 @@ public class ItemFactory {
     private ItemFactory() {};
     
     static public Item MakeItem(String itemType) {
-        if (itemType.equalsIgnoreCase("PAPERSCORE")) {
-            return new PaperScore();
-        } else if (itemType.equalsIgnoreCase("CD")) {
-            return new CD();
-        } else if (itemType.equalsIgnoreCase("VINYL")) {
-            return new Vinyl();
-        } else if (itemType.equalsIgnoreCase("CASSETTE")) {
-            return new Cassette();
-        } else if (itemType.equalsIgnoreCase("CDPLAYER")) {
-            return new CDPlayer();
-        } else if (itemType.equalsIgnoreCase("RECORDPLAYER")) {
-            return new RecordPlayer();
-        } else if (itemType.equalsIgnoreCase("MP3PLAYER")) {
-            return new MP3Player();
-        } else if (itemType.equalsIgnoreCase("CASSETTEPLAYER")) {
-            return new CassettePlayer();
-        } else if (itemType.equalsIgnoreCase("GUITAR")) {
-            return new Guitar();
-        } else if (itemType.equalsIgnoreCase("BASS")) {
-            return new Bass();
-        } else if (itemType.equalsIgnoreCase("MANDOLIN")) {
-            return new Mandolin();
-        } else if (itemType.equalsIgnoreCase("FLUTE")) {
-            return new Flute();
-        } else if (itemType.equalsIgnoreCase("HARMONICA")) {
-            return new Harmonica();
-        } else if (itemType.equalsIgnoreCase("SAXOPHONE")) {
-            return new Saxophone();
-        } else if (itemType.equalsIgnoreCase("HATS")) {
-            return new Hats();
-        } else if (itemType.equalsIgnoreCase("SHIRTS")) {
-            return new Shirts();
-        } else if (itemType.equalsIgnoreCase("BANDANAS")) {
-            return new Bandanas();
-        } else if (itemType.equalsIgnoreCase("PRACTICEAMPS")) {
-            return new PracticeAmps();
-        } else if (itemType.equalsIgnoreCase("CABLES")) {
-            return new Cables();
-        } else if (itemType.equalsIgnoreCase("STRINGS")) {
-            return new Strings();
-        } else if (itemType.equalsIgnoreCase("GIGBAG")) {
-            return new GigBag();
-        } 
+        if (itemType.equalsIgnoreCase("PAPERSCORE")) return new PaperScore();
+        else if (itemType.equalsIgnoreCase("CD")) return new CD();
+        else if (itemType.equalsIgnoreCase("VINYL")) return new Vinyl();
+        else if (itemType.equalsIgnoreCase("CASSETTE")) return new Cassette();
+        else if (itemType.equalsIgnoreCase("CDPLAYER")) return new CDPlayer();
+        else if (itemType.equalsIgnoreCase("RECORDPLAYER")) return new RecordPlayer();
+        else if (itemType.equalsIgnoreCase("MP3PLAYER")) return new MP3Player();
+        else if (itemType.equalsIgnoreCase("CASSETTEPLAYER")) return new CassettePlayer();
+        else if (itemType.equalsIgnoreCase("GUITAR")) return new Guitar();
+        else if (itemType.equalsIgnoreCase("BASS")) return new Bass();
+        else if (itemType.equalsIgnoreCase("MANDOLIN")) return new Mandolin();
+        else if (itemType.equalsIgnoreCase("FLUTE")) return new Flute();
+        else if (itemType.equalsIgnoreCase("HARMONICA")) return new Harmonica();
+        else if (itemType.equalsIgnoreCase("SAXOPHONE")) return new Saxophone();
+        else if (itemType.equalsIgnoreCase("HATS")) return new Hats();
+        else if (itemType.equalsIgnoreCase("SHIRTS")) return new Shirts();
+        else if (itemType.equalsIgnoreCase("BANDANAS")) return new Bandanas();
+        else if (itemType.equalsIgnoreCase("PRACTICEAMPS")) return new PracticeAmps();
+        else if (itemType.equalsIgnoreCase("CABLES")) return new Cables();
+        else if (itemType.equalsIgnoreCase("STRINGS")) return new Strings();
+        else if (itemType.equalsIgnoreCase("GIGBAG")) return new GigBag();  
         System.out.println("ERROR: ItemFactory given bad value : \"" + itemType + "\" : Item not instantiated");
         return null;
     }
+}
+
+// Abstract factory pattern to create components for the guitar kits
+interface KitFactory {
+    Pickups CreatePickups();
+    Pickguard CreatePickguard();
+    Covers CreateCovers();
+    KnobSet CreateKnobSet();
+    Bridge CreateBridge();
+    Neck CreateNeck();
+}
+
+class NorthKitFactory implements KitFactory {
+    public Pickups CreatePickups() { return new NorthPickups(); };
+    public Pickguard CreatePickguard() { return new NorthPickguard(); };
+    public Covers CreateCovers() { return new NorthCovers(); };
+    public KnobSet CreateKnobSet() { return new NorthKnobSet(); };
+    public Bridge CreateBridge() { return new NorthBridge(); };
+    public Neck CreateNeck() { return new NorthNeck(); };
+}
+
+class SouthKitFactory implements KitFactory {
+    public Pickups CreatePickups() { return new SouthPickups(); };
+    public Pickguard CreatePickguard() { return new SouthPickguard(); };
+    public Covers CreateCovers() { return new SouthCovers(); };
+    public KnobSet CreateKnobSet() { return new SouthKnobSet(); };
+    public Bridge CreateBridge() { return new SouthBridge(); };
+    public Neck CreateNeck() { return new SouthNeck(); };
 }
